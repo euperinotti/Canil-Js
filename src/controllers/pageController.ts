0a06e315ab38ac8a83a944146eb7a9241ec681de
Guilherme Perinotti <guilherme9115@hotmail.com>
2022-10-28 09:55:08 -0300

    ativando o menu

--- src/controllers/pageController.ts
+++ src/controllers/pageController.ts
@@ -2,6 +2,12 @@ import { Request, Response } from "express";
 
 export const home = (req: Request, res: Response) => {
     res.render('pages/page', {
+        menu: {
+            all: true,
+            dog: false,
+            cat: false,
+            fish: false
+        },
         banner: {
             title: 'Todos os animais',
             background: 'allanimals.jpg'
@@ -13,6 +19,12 @@ export const home = (req: Request, res: Response) => {
 
 export const dogs = (req: Request, res: Response) => {
     res.render('pages/page', {
+        menu: {
+            all: false,
+            dog: true,
+            cat: false,
+            fish: false
+        },
         banner: {
             title: 'Cachorros',
             background: 'banner_dog.jpg'
@@ -22,6 +34,12 @@ export const dogs = (req: Request, res: Response) => {
 
 export const cats = (req: Request, res: Response) => {
     res.render('pages/page', {
+        menu: {
+            all: false,
+            dog: false,
+            cat: true,
+            fish: false
+        },
         banner: {
             title: 'Gatos',
             background: 'banner_cat.jpg'
@@ -31,6 +49,12 @@ export const cats = (req: Request, res: Response) => {
 
 export const fishes = (req: Request, res: Response) => {
     res.render('pages/page', {
+        menu: {
+            all: false,
+            dog: false,
+            cat: false,
+            fish: true
+        },
         banner: {
             title: 'Peixes',
             background: 'banner_fish.jpg'
