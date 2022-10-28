7922a234f841180fc505bbde286ea7206c4f19cb
Guilherme Perinotti <guilherme9115@hotmail.com>
2022-10-27 22:11:12 -0300

    pre configuracao do server

---
+++ src/server.ts
@@ -0,0 +1,17 @@
+import express from 'express';
+import dotenv from 'dotenv';
+import mustache from 'mustache-express';
+import path from 'path';
+
+dotenv.config();
+
+const app = express();
+
+app.set('view engine', mustache);
+app.set('views', path.join(__dirname, 'views'));
+app.engine('mustache', mustache());
+
+app.use(express.static(path.join(__dirname, '../public')));
+
+
+app.listen(process.env.PORT);
