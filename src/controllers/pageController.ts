69f9616ee4f6b789c005a9374a0fddd897f78e35
Guilherme Perinotti <guilherme9115@hotmail.com>
2022-10-27 23:31:39 -0300

    modificand dados dinamicamente

--- src/controllers/pageController.ts
+++ src/controllers/pageController.ts
@@ -1,19 +1,39 @@
 import { Request, Response } from "express";
 
 export const home = (req: Request, res: Response) => {
-    res.render('pages/page');
+    res.render('pages/page', {
+        banner: {
+            title: 'Todos os animais',
+            background: 'allanimals.jpg'
+        }
+    });
 
     // res.send('Home no controller');
 }
 
 export const dogs = (req: Request, res: Response) => {
-    res.send('Dogs no controller');
+    res.render('pages/page', {
+        banner: {
+            title: 'Cachorros',
+            background: 'banner_dog.jpg'
+        }
+    });
 }
 
 export const cats = (req: Request, res: Response) => {
-    res.send('Cats no controller');
+    res.render('pages/page', {
+        banner: {
+            title: 'Gatos',
+            background: 'banner_cat.jpg'
+        }
+    });
 }
 
 export const fishes = (req: Request, res: Response) => {
-    res.send('Fishes no controller');
+    res.render('pages/page', {
+        banner: {
+            title: 'Peixes',
+            background: 'banner_fish.jpg'
+        }
+    });
 }
